c137e100b49cae6ead46ff11375ffa417d8e71fa
Qalandar Axmedov <64014854+QalandarAxmedov@users.noreply.github.com>
2020-09-13 00:50:38 +0500

    Create Begin__10.cs

---
+++ Begin__10.cs
@@ -0,0 +1,29 @@
+using System;
+
+namespace Abramyan_1000
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double a, b;
+            Console.Write("a=");
+            a = double.Parse(Console.ReadLine());
+            Console.Write("b=");
+            b = double.Parse(Console.ReadLine());
+            if (a != 0 && b != 0)
+            {
+                a *= a;
+                b *= b;
+                double sum = a + b;
+                double ayirma = a - b;
+                double kopaytma = a * b;
+                double nisbat = a / b;
+                Console.WriteLine($"Yigindi={sum}");
+                Console.WriteLine($"Ayirma={ayirma}");
+                Console.WriteLine($"Kopaytma={kopaytma}");
+                Console.WriteLine($"Nisbat={nisbat}");
+            }
+        }
+    }
+}
