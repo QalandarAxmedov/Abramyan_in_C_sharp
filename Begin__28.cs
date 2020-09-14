2d2c8658cbe0635194643b08b152e9fc63d9f53e
QalandarAxmedov <QalandarAxmedov@users.noreply.github.com>
2020-09-14 18:06:59 +0500

    Create Begin__28.cs

---
+++ Begin__28.cs
@@ -0,0 +1,24 @@
+using System;
+
+namespace Abramyan_1000
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double A,A2;
+            Console.Write("A=");
+            A = double.Parse(Console.ReadLine());
+            A2 = A * A;
+            Console.WriteLine($"A^2={A}");
+            A = A2 * A;
+            Console.WriteLine($"A^3={A}");
+            A = A2 * A;
+            Console.WriteLine($"A^5={A}");
+            A2 = A * A;
+            Console.WriteLine($"A^10={A}");
+            A = A2 * A;
+            Console.WriteLine($"A^15={A}");
+        }
+    }
+}
