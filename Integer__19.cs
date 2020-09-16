384186e825904800ba4d21f476931ecbaab40ca4
QalandarAxmedov <QalandarAxmedov@users.noreply.github.com>
2020-09-16 21:20:41 +0500

    Create Integer__19.cs

---
+++ Integer__19.cs
@@ -0,0 +1,16 @@
+using System;
+
+namespace Abramyan_1000
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int N,n;
+            Console.Write("sekund N=");
+            N = int.Parse(Console.ReadLine());
+            n = N / 60;
+            Console.Write($"{N} sekund = {n} minut");
+        }
+    }
+}
