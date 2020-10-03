c36b708cd7701aec3df3e9d2cd3810e075069f22
QalandarAxmedov <QalandarAxmedov@users.noreply.github.com>
2020-10-03 14:48:36 +0500

    Create Boolean__12.cs

---
+++ Boolean__12.cs
@@ -0,0 +1,21 @@
+using System;
+
+namespace Abramyan_1000
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int A, B, C;
+            bool Natija;
+            Console.Write("A=");
+            A = int.Parse(Console.ReadLine());
+            Console.Write("B=");
+            B = int.Parse(Console.ReadLine());
+            Console.Write("C=");
+            C = int.Parse(Console.ReadLine());
+            Natija = A > 0 & B > 0 & C > 0;
+            Console.Write($"{Natija}");
+        }
+    }
+}
