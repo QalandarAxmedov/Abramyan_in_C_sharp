0c94af017ea9521622ddf5215c280f110a9e1128
QalandarAxmedov <QalandarAxmedov@users.noreply.github.com>
2020-10-03 14:25:54 +0500

    Create Boolean_8.cs

---
+++ Boolean_8.cs
@@ -0,0 +1,20 @@
+using System;
+
+namespace Abramyan_1000
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int A, B;
+            bool Natija;
+            Console.Write("A=");
+            A = int.Parse(Console.ReadLine());
+            Console.Write("B=");
+            B = int.Parse(Console.ReadLine());
+
+            Natija = A % 2 == 1 & B % 2 == 1;
+            Console.Write($"{Natija}");
+        }
+    }
+}
